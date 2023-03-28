f6debad33b12a3901859772d3d1bac3102b71273
HosneMobarak262 <hosne.1504@gmail.com>
2023-03-28 20:06:20 +0600

    34. Create a function that will return an array with words inside a text

--- faisal_vai_HW.js
+++ faisal_vai_HW.js
@@ -618,3 +618,31 @@ function sumCSV(text){
     }
     return sum;
 }
+
+// Coding challenge #34. Create a function that will return an array with words inside a text
+
+var text = "Create a function, that will return an array (of string), with the words inside the text";
+
+console.log(getWords(text));
+
+function getWords(text) {
+    let startWord = -1;
+    let array = [];
+
+    for (let i = 0; i <= text.length; i++) {
+        let c = i < text.length ? text[i] : " ";
+
+        if (!isSeperator(c) && startWord < 0) {
+            startWord = i;
+        }
+
+        if (isSeperator(c) && startWord >= 0) {
+            let word = text.substring(startWord, i);
+            array.push(word);
+
+            startWord = -1;
+        }
+    }
+
+    return array;
+}

107f49964a8e1594a407cbb868deb52eccacf0c6
HosneMobarak262 <hosne.1504@gmail.com>
2023-03-28 20:23:51 +0600

    38. Create a function that will convert an array containing ASCII codes in a string

--- faisal_vai_HW.js
+++ faisal_vai_HW.js
@@ -685,3 +685,10 @@ function getCharCodes(str) {
 
     return array;
 }
+
+// Coding challenge #38. Create a function that will convert an array containing ASCII codes in a string
+console.log(codesToString([73, 32, 108, 105, 107, 101, 32, 74, 97, 118, 97, 83, 99, 114, 105, 112, 116]));
+
+function codesToString(codes){
+    return String.fromCharCode(...codes);
+}

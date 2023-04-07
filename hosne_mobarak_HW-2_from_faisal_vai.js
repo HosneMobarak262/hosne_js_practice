2e6c8c25508aa4312471460e15f801a49c7f27c8
HosneMobarak262 <hosne.1504@gmail.com>
2023-04-08 00:05:49 +0600

    hw 2 upto  problem 10

---
+++ hosne_mobarak_HW-2_from_faisal_vai.js
@@ -0,0 +1,111 @@
+// Array generate
+const arr = [];
+for (var i = 0; i < 50; i++){
+    arr.push(Math.floor(Math.random() * 100));
+}
+
+// Problem 1: smallest number
+let smallestNum = Math.min();
+arr.forEach(element => {
+    if(smallestNum > element){
+        smallestNum = element;
+    }
+});
+console.log(`Array: ${arr}`)
+console.log(`Smallest Number: ${smallestNum}`);
+
+// Problem 2: largest number
+let largestNum = Math.max();
+arr.forEach(item => {
+    if(largestNum < item){
+        largestNum = item;
+    }
+});
+console.log(`Largest Number: ${largestNum}`);
+
+// Problem 3: sum of all numbers
+let sumOfNum = 0;
+arr.forEach(item =>{
+    sumOfNum += item;
+});
+console.log(`Sum of Numbers: ${sumOfNum}`);
+
+// Problem 4: Given an array of strings, create a new array with the first letter of each string
+const strings = ['hello', 'world', 'I', 'am', 'hosne', 'mobarak', 'hossain'];
+const firstLetters = [];
+
+strings.forEach(item => {
+    firstLetters.push(item[0]);
+});
+console.log(`First letter of each string: ${firstLetters}`);
+
+// Problem 5: create an array of even numbers from an array
+const evenArray = [];
+arr.filter(item => {
+    if(item % 2 == 0){
+        evenArray.push(item);
+    }
+});
+console.log(`Even array: ${evenArray}`);
+
+// Problem 6: longest several strings
+let longestString = [];
+let longestStringLength = -1;
+strings.forEach(item => {
+    var item_length = item.length;
+    if(item_length >= longestStringLength){
+        longestStringLength = item_length;
+        if(longestString.length != 0 ){
+            var preLen = longestString[0].length;
+            if ( preLen < longestStringLength){
+                longestString = [];
+                longestString.push(item);
+            } else {
+                longestString.push(item);
+            }
+        } else {
+            longestString.push(item);
+        }
+    }
+});
+console.log(`Longest length string: ${longestString}`);
+
+// Problem 7: Average of numbers
+console.log(`Average of Numbers: ${sumOfNum/arr.length}`);
+
+// Problem 8: Sort strings
+console.log(`String sort: ${strings.sort()}`)
+
+// Problem 9: remove duplicates from array
+const uniqueNumbers = [...new Set(arr)];
+console.log(`Unique numbers from an array: ${uniqueNumbers}`);
+
+// Problem 10: find two numbers that add up to the target value
+let target = 80;
+let arrLength = arr.length;
+for(var i = 0; i < arrLength; i++){
+    for (var j = i + 1; j < arrLength; j ++){
+        if(arr[i] + arr[j] === target){
+            console.log(`Target: ${target} = ${arr[i]} + ${arr[j]}`)
+        }
+    }
+}
+
+// Problem 10: using Hash
+console.log("Hashing Algorithm");
+function printPairs(arr, sum){
+    let hashArr = [];
+    let temp = 0;
+    arr.forEach(item => {
+        temp = sum - item;
+        var hashArrLen = hashArr.length;
+        for(var i = 0; i < hashArrLen ; i++){
+            if(arr[i] == temp){
+                console.log(`Target ${target} = ${temp} + ${item}`);
+            }
+        }
+        hashArr.push(item);
+    });
+}
+// I wasn't expecting this code to work but somehow it is working
+printPairs(arr, target);

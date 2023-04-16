cd7b892d93edbf80d5134929a74e1a64e92f8bf5
HosneMobarak262 <hosne.1504@gmail.com>
2023-04-16 14:02:12 +0600

    faisal vais assignment 2 upto problem 16

--- hosne_mobarak_HW-2_from_faisal_vai.js
+++ hosne_mobarak_HW-2_from_faisal_vai.js
@@ -137,3 +137,195 @@ userPosts.forEach(post => {
     });
 });
 console.log(hashTagArray);
+
+// Problem 12: using regular expression
+
+// const hashArr = new Set();
+
+// userPosts.forEach((post) => {
+//     let regex = /#\w+/g;
+//     let matches = post.content.match(regex);
+
+//     if(matches){
+//         matches.forEach((item) => {
+//             hashArr.add(item.slice(1))
+//         });
+//     }
+// });
+
+// console.log(hashArr);
+
+// Error for Above code
+
+// let matches = post.content.match(regex);
+// match Error                ^
+
+// TypeError: Cannot read properties of undefined (reading 'match')
+// at D:\ostad\jspractice\hosne_mobarak_HW-2_from_faisal_vai.js:146:32
+// at Array.forEach (<anonymous>)
+// at Object.<anonymous> (D:\ostad\jspractice\hosne_mobarak_HW-2_from_faisal_vai.js:144:11)
+// at Module._compile (node:internal/modules/cjs/loader:1275:14)
+// at Module._extensions..js (node:internal/modules/cjs/loader:1329:10)
+// at Module.load (node:internal/modules/cjs/loader:1133:32)
+// at Module._load (node:internal/modules/cjs/loader:972:12)
+// at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:83:12)
+// at node:internal/main/run_main_module:23:47
+
+
+// Problem 13: weather app
+
+const temperatures = [15, 65, 24, 36, 98];
+
+const formattedTemp = temperatures.map((temp) => {
+    const fahrenheit = temp * 1.8 + 32;
+    return `${temp}'C = ${fahrenheit}'F`
+});
+
+console.log(formattedTemp);
+
+// Problem 14: video sharing site
+
+const videos = [
+    {
+        id: "vid001",
+        title: "How to Code a React App",
+        views: 2000,
+        likes: 60,
+        comments: [
+            { id: "c1", text: "Nice Tutorial" },
+            { id: "c2", text: "Thanks for sharing"},
+        ],
+    },
+    {
+        id: "vid002",
+        title: "Building a REST API with Node.js",
+        views: 1500,
+        likes: 30,
+        comments: [
+            { id: "c3", text: "Very helpful, thanks"},
+            { id: "c4", text: "Can't wait to try this out."},
+        ],
+    },
+];
+
+// increment the view count of a video
+function incrementViewCount(videoId){
+    const video = videos.find((vid) => {
+        return vid.id === videoId
+    });
+    video.views += 1;
+}
+
+// add comment to a video
+function addComment(videoId, comment){
+    const video = videos.find((vid) => {
+        return vid.id === videoId;
+    });
+    video.comments.push(comment);
+}
+
+// increment like of a video
+function incrementLike(videoId){
+    const video = videos.find((vid) => vid.id === videoId );
+    video.likes += 1;
+}
+
+// when using arrow function, if we are using {}, then it must return something but if we are not using {} and everything is done in a single line, then we don't have to use return.
+
+incrementViewCount("vid001");
+addComment("vid002", { id: "c5", text: "Excellent tutorial."});
+incrementLike("vid002");
+
+console.log(videos);
+
+// Problem 15: Facebook reactions
+
+const posts = [
+    {
+        id: "_1",
+        author: "Mobarak",
+        content: "JS 100 problem basics",
+        reactions: {
+            like: 60,
+            love: 45,
+            haha: 8,
+            wow: 4,
+            sad: 2,
+            angry: 0,
+        },
+    },
+    {
+        id: "_2",
+        author: "janina",
+        reactions: {
+            like: 100,
+            love: 90,
+            haha: 80,
+            wow: 70,
+            sad: 10,
+            angry: 5,
+        }
+    },
+];
+
+function incrementReactionCount(postId, reactionType){
+    try {
+        const post = posts.find((item) => item.id === postId);
+        post.reactions[reactionType] += 1;
+        console.log(`Incremented ${reactionType} Reaction count for post ${postId}` );    
+    } catch (error) {
+        console.log(`ID --> ${postId} not found with error: ${error}`);
+    }
+    
+}
+
+// testing
+incrementReactionCount("_1", "love");
+incrementReactionCount("_2", "angry");
+incrementReactionCount("_3", "like");
+
+console.log(posts);
+
+// Problem 16: find two numbers that add up to a certain value
+
+const purchases = [1,2,3,4,5,6,1,7,8,9,6,5,8,9];
+let maxSum = Math.max(...purchases);
+let minSum = Math.min(...purchases);
+
+maxSum = maxSum << 1;
+minSum = minSum << 1;
+
+console.log(maxSum, minSum);
+
+ let targetLargest = 0;
+ let targetCount = 0;
+
+function targetCheck(target){
+    let pairs = [];
+    let count = 0;
+    let purchaseLen = purchases.length;
+
+    console.log(`Target: ${target}`);
+    for(let i = 0; i < purchaseLen; i++){
+        for(let j = i + 1; j < purchaseLen; j++){
+            if(purchases[i] + purchases[j] === target){
+                pairs.push([purchases[i], purchases[j]] );
+                ++count;
+            }
+        }
+    }
+    if (targetCount < count) {
+        targetLargest = target;
+        targetCount = count;
+    }
+    console.log(pairs );
+    console.log(`Number of pairs: ${count}`);
+}
+
+for (var i = minSum; i <= maxSum; i++ ){
+    targetCheck(i);
+}
+
+console.log(`Largest target: ${targetLargest}`);
+console.log(`Largest count: ${targetCount}`);
+

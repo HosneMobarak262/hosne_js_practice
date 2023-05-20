fc3187e3ad5098b4dfec5d1a6611402b1833ea16
HosneMobarak262 <hosne.1504@gmail.com>
2023-05-20 16:46:19 +0600

    module 6 con 2 app 5 basic routes

--- NodePractice/mod-6-2/server.js
+++ NodePractice/mod-6-2/server.js
@@ -13,9 +13,30 @@ const app = express();
 //     res.send("<h1>hello express js home path</h1>");
 // });
 
-app.all("/ostad", (req, res) =>{
-    res.send("<h1>hello express js Ostad path</h1>");
+// app.all("/ostad", (req, res) =>{
+//     res.send("<h1>hello express js Ostad path</h1>");
+// });
+
+app.get("/", (req, res)=>{
+    res.send("<h1>Hello express js get page</h1>");
 });
+
+app.post("/", (req, res)=>{
+    res.send("<h1>Hello express js post page</h1>")
+});
+
+app.put("/", (req, res) =>{
+    res.send("<h1>Hello express js put page</h1>");
+});
+
+app.patch("/", (req, res) => {
+    res.send("<h1>Hello express js patch page</h1>");
+});
+
+app.delete("/", (req, res) => {
+    res.send("<h1>Hello express js delete page</h1>");
+});
+
 
 app.listen(port, ()=>{
     console.log("Server running on port: ", port);

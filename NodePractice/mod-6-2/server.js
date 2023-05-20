b845dbac44f73c12119ca0bea6444a1f437f6735
HosneMobarak262 <hosne.1504@gmail.com>
2023-05-20 14:05:56 +0600

    module 6 con 2 app home path

--- NodePractice/mod-6-2/server.js
+++ NodePractice/mod-6-2/server.js
@@ -5,8 +5,12 @@ const port = 5050;
 const app = express();
 // console.log(app);
 
-app.all("*", (req, res) =>{
-    res.send("<h1>hello express</h1>");
+// app.all("*", (req, res) =>{
+//     res.send("<h1>hello express</h1>");
+// });
+
+app.all("/", (req, res) =>{
+    res.send("<h1>hello express js home path</h1>");
 });
 
 app.listen(port, ()=>{
